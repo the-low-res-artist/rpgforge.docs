4be8054d41b422d25137bb8bc49028c393178bc0
the-low-res-artist <pixelart.asset@gmail.com>
2023-11-19 19:32:13 +0100

    rebuild

--- rebuild_website.sh
+++ rebuild_website.sh
@@ -1,3 +1,5 @@
+# TODO attention probleme de git pull, de build (shopt not found)
+
 # ---------------------------------------------------------------
 # variables
 root_dir=${PWD}
@@ -11,17 +13,21 @@ cd ${root_dir}/scripts && python3 -m glossary.py && cd ${root_dir}
 # ---------------------------------------------------------------
 # BUILD SCRIPTS
 # find all the book.toml files and execute the build
-shopt -s globstar
-for i in src/**/book.toml; do # Whitespace-safe and recursive
-    filepath="$i"
-    src_folderpath=${root_dir}/$(dirname "${filepath}")
-    dest_folderpath=${src_folderpath/src/book}
-    echo ==========================================================
-    echo mdbook build ${src_folderpath} -d ${dest_folderpath}
-    mdbook build ${src_folderpath} -d ${dest_folderpath}
-    ln -s ${root_dir}/media/ ${root_dir}/book/en/stable/media
-    ln -s ${root_dir}/scripts/ ${root_dir}/book/en/stable/js
-done
+mdbook build /var/www/com.rpgforge/public/src/en/stable -d /var/www/com.rpgforge/public/book/en/stable
+ln -s /var/www/com.rpgforge/public/media/ /var/www/com.rpgforge/public/book/en/stable/media
+ln -s /var/www/com.rpgforge/public/scripts/ /var/www/com.rpgforge/public/book/en/stable/js
+
+#shopt -s globstar
+#for i in src/**/book.toml; do # Whitespace-safe and recursive
+#    filepath="$i"
+#    src_folderpath=${root_dir}/$(dirname "${filepath}")
+#    dest_folderpath=${src_folderpath/src/book}
+#    echo ==========================================================
+#    echo mdbook build ${src_folderpath} -d ${dest_folderpath}
+#    mdbook build ${src_folderpath} -d ${dest_folderpath}
+#    ln -s ${root_dir}/media/ ${root_dir}/book/en/stable/media
+#    ln -s ${root_dir}/scripts/ ${root_dir}/book/en/stable/js
+#done
 
 # ---------------------------------------------------------------
 # POST BUILD SCRIPTS
