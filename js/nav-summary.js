e529049e5af22fd48dcfc03cf9550519376a59a0
the-low-res-artist <pixelart.asset@gmail.com>
2023-11-26 15:27:02 +0100

    fix

--- js/nav-summary.js
+++ js/nav-summary.js
@@ -1,8 +1,14 @@
-function toggle_section(element) {
+"use strict";
+
+// Fix back button cache problem
+window.onunload = function () { };
+window.toggle_section = toggle_section;
+
+(function toggle_section(element) {
     var item = element.parentNode; // get the parent 'li' element
     if (item.className === "part-title") {
     	section.className = "part-title expanded";
     } else {
     	section.className = "part-title";
     }
-};
+})();
