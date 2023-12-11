ab492de8bb682225e49dadbfe01d67ebcb5b3ac5
the-low-res-artist <pixelart.asset@gmail.com>
2023-12-11 12:26:00 +0100

    expand

--- scripts/nav-summary.py
+++ scripts/nav-summary.py
@@ -29,24 +29,39 @@ def set_nav_summary(filename, current_chapter):
 
     # reformat some chapters
     # Home
-    str_to_replace="<div class=\"sidebar-scrollbox\"><ol class=\"chapter\"><li class=\"chapter-item expanded affix \">"
-    str_replacement="<div class=\"sidebar-scrollbox\"><ol class=\"chapter\"><li class=\"part-title expanded affix \">"
-    chapters_new = chapters_new.replace(str_to_replace, str_replacement)
-    # 1. Installation (show)
+    #str_to_replace="<li class=\"chapter-item expanded affix \"><a href=\"front_page.html\">Home</a></li>"
+    #str_replacement="<li class=\"part-title expanded affix\"><a href=\"front_page.html\">Home <i class=\"fa fa-home\"></i></a></li>"
+    #chapters_new = chapters_new.replace(str_to_replace, str_replacement)
+    
+    #str_to_replace="<div class=\"sidebar-scrollbox\"><ol class=\"chapter\"><li class=\"chapter-item expanded affix \">"
+    #str_replacement="<div class=\"sidebar-scrollbox\"><ol class=\"chapter\"><li class=\"part-title expanded affix \">"
+    #chapters_new = chapters_new.replace(str_to_replace, str_replacement)
+    
+    # 1. Installation
+    expand = "expanded"
     str_to_replace="<li class=\"chapter-item expanded \"><div><strong aria-hidden=\"true\">1.</strong> Installation</div></li>"
-    str_replacement="<li class=\"part-title expanded\" id=\"li-installation\"><button class=\"button-summary\" id=\"button-installation\" title=\"Installation\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-down\" id=\"chevron-installation\"></i>&nbsp;&nbsp;Installation&nbsp;&nbsp;<i class=\"fa fa-download\"></i></strong></button></li>"
+    str_replacement=f"<li class=\"part-title {expand}\" id=\"li-installation\"><button class=\"button-summary\" id=\"button-installation\" title=\"Installation\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-down\" id=\"chevron-installation\"></i>&nbsp;&nbsp;Installation&nbsp;&nbsp;<i class=\"fa fa-download\"></i></strong></button></li>"
     chapters_new = chapters_new.replace(str_to_replace, str_replacement)
-    # 2. Getting started (hide)
+    # 2. Getting started
+    expand = ""
+    if ("getting_started" in current_chapter):
+        expand = "expanded"
     str_to_replace="<li class=\"chapter-item expanded \"><div><strong aria-hidden=\"true\">2.</strong> Getting started</div></li>"
-    str_replacement="<li class=\"part-title\" id=\"li-getting-started\"><button class=\"button-summary\" id=\"button-getting-started\" title=\"Getting Started\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-getting-started\"></i>&nbsp;&nbsp;Getting started&nbsp;&nbsp;<i class=\"fa fa-bolt\"></i></strong></button></li>"
+    str_replacement=f"<li class=\"part-title {expand}\" id=\"li-getting-started\"><button class=\"button-summary\" id=\"button-getting-started\" title=\"Getting Started\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-getting-started\"></i>&nbsp;&nbsp;Getting started&nbsp;&nbsp;<i class=\"fa fa-bolt\"></i></strong></button></li>"
     chapters_new = chapters_new.replace(str_to_replace, str_replacement)
-    # 3. User manual (hide)
+    # 3. User manual
+    expand = ""
+    if ("user_manual" in current_chapter):
+        expand = "expanded"
     str_to_replace="<li class=\"chapter-item expanded \"><div><strong aria-hidden=\"true\">3.</strong> User manual</div></li>"
-    str_replacement="<li class=\"part-title\" id=\"li-user-manual\"><button class=\"button-summary\" id=\"button-user-manual\" title=\"User Manual\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-user-manual\"></i>&nbsp;&nbsp;User manual&nbsp;&nbsp;<i class=\"fa fa-book\"></i></strong></button></li>"
+    str_replacement=f"<li class=\"part-title {expand}\" id=\"li-user-manual\"><button class=\"button-summary\" id=\"button-user-manual\" title=\"User Manual\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-user-manual\"></i>&nbsp;&nbsp;User manual&nbsp;&nbsp;<i class=\"fa fa-book\"></i></strong></button></li>"
     chapters_new = chapters_new.replace(str_to_replace, str_replacement)
     # 4. Community (hide)
+    expand = ""
+    if ("community" in current_chapter):
+        expand = "expanded"
     str_to_replace="<li class=\"chapter-item expanded \"><div><strong aria-hidden=\"true\">4.</strong> Community</div></li>"
-    str_replacement="<li class=\"part-title\" id=\"li-community\"><button class=\"button-summary\" id=\"button-community\" title=\"Community\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-community\"></i>&nbsp;&nbsp;Community&nbsp;&nbsp;<i class=\"fa fa-comments\"></i></strong></button></li>"
+    str_replacement=f"<li class=\"part-title {expand}\" id=\"li-community\"><button class=\"button-summary\" id=\"button-community\" title=\"Community\"><strong aria-hidden=\"true\"><i class=\"fa fa-chevron-right\" id=\"chevron-community\"></i>&nbsp;&nbsp;Community&nbsp;&nbsp;<i class=\"fa fa-comments\"></i></strong></button></li>"
     chapters_new = chapters_new.replace(str_to_replace, str_replacement)
 
     # update the nav section (chapters)
@@ -55,8 +70,8 @@ def set_nav_summary(filename, current_chapter):
     s = s.replace(str_to_replace, str_replacement) 
 
     # Safely write the changed content
-    with open(filename, 'w', encoding="utf8") as f:
-        f.write(''.join(s))
+    #with open(filename, 'w', encoding="utf8") as f:
+    #    f.write(''.join(s))
 
 # entry point
 book_root = "./../book/"
@@ -65,7 +80,8 @@ print("====================================")
 print("NAV SUMMARY UPDATE")
 print(f"Scanning html files in {book_root} and update the navigation summary section")
 for root, dirs, files in os.walk(book_root, topdown=False):
-   current_chapter=""
+   print(root)
+   current_chapter=root.replace(book_root, "").split("/")[0]
    for filename in files:
         if filename.endswith(".html"):
             set_nav_summary(os.path.join(root, filename), current_chapter)
