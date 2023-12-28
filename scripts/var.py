41d84dc88a55bb34b1f8990fa6810bd4ba5a74d0
the-low-res-artist <pixelart.asset@gmail.com>
2023-12-28 11:02:54 +0100

    no footer

---
+++ scripts/var.py
@@ -0,0 +1,7 @@
+# define global variables used in python pre and post build scripts
+class var:
+
+    # links
+    link_x = "https://twitter.com/RPGPowerForge"
+    link_discord = "https://onesquareminesweeper.com/"
+    link_trello = "https://trello.com/b/PIzgsYov/road-map"
