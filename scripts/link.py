79657ea78d8f239cfc9d7f410c01dc02a81ef3f2
the-low-res-artist <pixelart.asset@gmail.com>
2023-12-28 17:16:06 +0100

    link

--- scripts/link.py
+++ scripts/link.py
@@ -2,8 +2,8 @@
 import re # regex operations
 import sys # to return 0
 import os # loop over files
-
-# goal : edit the external links by removing ".html" at the end
+from config import config
+# goal : edit the external links
 
 # replace in a file
 def set_link(filename):
@@ -15,33 +15,25 @@ def set_link(filename):
     # safe exit
     if (s == ""):
         return
-
-    # links can be reformatted by mdbook (depending on where the html page is in the hierarchy) :
-    # https://twitter.com/RPGPowerForge.html
-    # ../https://twitter.com/RPGPowerForge.html
-    # ../../https://twitter.com/RPGPowerForge.html
-    # ...
     
     # I add 'target=\"_blank\"' to make the click open a new tab
 
-    # lets' go up to 10 dept levels
-    for i in range(10):
-        # current dept level
-        dept= i * '../'
-        
-        # Twitter / X
-        str_to_replace = f"<a href=\"{dept}https://twitter.com/RPGPowerForge.html\""
-        str_replacement = "<a href=\"https://twitter.com/RPGPowerForge\" target=\"_blank\""
-        s = s.replace(str_to_replace, str_replacement)
+    # Discord
+    str_to_replace = "<div><strong aria-hidden=\"true\">4.1.</strong> link_discord</div>"
+    str_replacement = f"<a href=\"{config.link_discord}\" target=\"_blank\"><strong aria-hidden=\"true\">4.1.</strong> Discord server</a>"
+    str_replacement = "<a href=\"https://trello.com/b/PIzgsYov/rpg-power-forge-road-map\" target=\"_blank\""
+    s = s.replace(str_to_replace, str_replacement)
 
-        str_to_replace = f"<a href=\"{dept}https://x.com/RPGPowerForge.html\""
-        str_replacement = "<a href=\"https://x.com/RPGPowerForge\" target=\"_blank\""
-        s = s.replace(str_to_replace, str_replacement)
+    # Twitter / X
+    str_to_replace = "<div><strong aria-hidden=\"true\">4.2.</strong> link_x</div><"
+    str_replacement = f"<a href=\"{config.link_x}\" target=\"_blank\"><strong aria-hidden=\"true\">4.2.</strong> Twitter / X</a>"
+    s = s.replace(str_to_replace, str_replacement)
 
-        # Trello
-        str_to_replace = f"<a href=\"{dept}https://trello.com/b/PIzgsYov/rpg-power-forge-road-map.html\""
-        str_replacement = "<a href=\"https://trello.com/b/PIzgsYov/rpg-power-forge-road-map\" target=\"_blank\""
-        s = s.replace(str_to_replace, str_replacement)
+    # Trello
+    str_to_replace = "<div><strong aria-hidden=\"true\">4.3.</strong> link_trello</div>"
+    str_replacement = f"<a href=\"{config.link_trello}\" target=\"_blank\"><strong aria-hidden=\"true\">4.3.</strong> Trello</a>"
+    s = s.replace(str_to_replace, str_replacement)
+
 
     # Safely write the changed content
     with open(filename, 'w', encoding="utf8") as f:
@@ -53,7 +45,7 @@ book_root = "./../book/"
 nb_files=0
 print("====================================")
 print("LINKS UPDATE")
-print(f"Scanning html files in {book_root} and fixing external links suffixes")
+print(f"Scanning html files in {book_root} and adding external links in nav summary")
 for root, dirs, files in os.walk(book_root, topdown=False):
    for filename in files:
         if filename.endswith(".html"):
