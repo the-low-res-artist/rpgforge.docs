246c94b9a58ea48903aff24263906c18b83c24ed
the-low-res-artist <pixelart.asset@gmail.com>
2024-02-24 17:00:06 +0100

    css

--- scripts/overview.py
+++ scripts/overview.py
@@ -26,37 +26,37 @@ def set_overview(filename):
       <div class=\"overview_asset_picker overview\"> \
         <div class=\"panel_asset_picker panel\">\
           <h3>Asset Picker</h3>\
-          <p>Import, edit, drag and drop various assets on the Scene ! Perfect for prefabs and tiles.</p>\
+          <p>Import, organize, edit, drag and drop all of your assets on the Scene !</p>\
         </div>\
       </div>\
       <div class=\"overview_toolbar overview\"> \
         <div class=\"panel_toolbar panel\">\
           <h3>Tool Bar</h3>\
-          <p>Quick access to most RPG Power Forge features. Data management, project settings, game build and more !</p>\
+          <p>Quick access to most RPG Power Forge features. Data, settings, build...</p>\
         </div>\
       </div>\
       <div class=\"overview_scene overview\"> \
         <div class=\"panel_scene panel\">\
           <h3>Scene</h3>\
-          <p>Customize the settings for each Scene element (tiles, props, actors, ...)</p>\
+          <p>This is where everything comes to life !</p>\
         </div>\
       </div>\
       <div class=\"overview_behavior overview\">\
         <div class=\"panel_behavior panel\">\
           <h3>Behaviors</h3>\
-          <p>Attach any high-level RPG behaviors to an Actor (prefab).</p>\
+          <p>Attach multiple high-level RPG game behaviors to an Actor (prefab).</p>\
         </div>\
       </div>\
       <div class=\"overview_properties overview\"> \
         <div class=\"panel_properties panel\">\
           <h3>Properties</h3>\
-          <p>Customize the settings for each Scene element (tiles, props, actors, ...)</p>\
+          <p>Customize the settings for each Scene element (tiles, props, Actors, ...)</p>\
         </div>\
       </div>\
       <div class=\"overview_hierarchy overview\"> \
         <div class=\"panel_hierarchy panel\">\
           <h3>Hierarchy</h3>\
-          <p>Select and reorganize your Scene elements (actors, props, ...)</p>\
+          <p>Select and reorganize your Scene elements (Actors, props, ...)</p>\
         </div>\
       </div>\
     </div>\
