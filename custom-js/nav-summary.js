0d97478cf2caa21f8169dbf6bb2d2eb7c9605b5e
the-low-res-artist <pixelart.asset@gmail.com>
2024-06-16 12:33:04 +0200

    test

--- custom-js/nav-summary.js
+++ custom-js/nav-summary.js
@@ -3,16 +3,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Add a click event listener to the <nav> element
     document.querySelector('nav').addEventListener('click', function(event) {
-        // Check if the clicked element is an <li> with the desired classes
-        console.log("click");
-        console.log(event);
-        console.log(event.target.tagName);
-        console.log(event.target.parent.tagName);
-        if (event.target.tagName === 'LI' && 
-            (event.target.classList.contains('chapter-item') || 
-             event.target.classList.contains('part-title'))) {
+        // Check if the clicked element parent is an <li> with the desired classes
+        var parent = event.target.parentElement
+        if (parent.tagName === 'LI' && 
+            (parent.classList.contains('chapter-item') || 
+            parent.classList.contains('part-title'))) {
             // Toggle the 'expanded' class on the clicked <li>
-            event.target.classList.toggle('expanded');
+            parent.classList.toggle('expanded');
         }
     });
 });
