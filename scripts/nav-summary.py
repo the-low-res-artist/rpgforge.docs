2f3f38066598f18f1e22e3b9ce0abbe64bc8d3a6
the-low-res-artist <pixelart.asset@gmail.com>
2024-06-16 14:24:52 +0200

    css

--- scripts/nav-summary.py
+++ scripts/nav-summary.py
@@ -23,15 +23,13 @@ def set_nav_summary(filename):
 
     # get the chapters in the nav section
     number_chapters = re.findall(NUMBER_CHAPTER_REGEX, s)
-    print(number_chapters)
     # safe exit
     if (len(number_chapters) == 0):
         return
 
     # remove numbers in chapter title
     for number in number_chapters:
-        print(number)
-        str_to_replace = number
+        str_to_replace = number[0] # first element of the regex tuple
         str_replacement = ""
         s = s.replace(str_to_replace, str_replacement) 
 
