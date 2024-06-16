bbbe786c2ba5c105ec6a28155dc27325d18ddbac
the-low-res-artist <pixelart.asset@gmail.com>
2024-06-16 14:59:30 +0200

    chevron

--- custom-js/nav-summary.js
+++ custom-js/nav-summary.js
@@ -20,12 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Create an SVG element
     const svgIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svgIcon.setAttribute('class', 'icon');
-    svgIcon.setAttribute('viewBox', '0 0 24 24'); // Adjust viewBox as per your SVG icon dimensions
+    svgIcon.setAttribute('viewBox', '0 0 512 512'); // Adjust viewBox as per your SVG icon dimensions
     svgIcon.setAttribute('width', '26'); // max size
 
     // Create and append SVG path (replace with your SVG path data)
     const svgPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    svgPath.setAttribute('d', 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm-2 14H8v-2h2v2zm0-4H8v-4h2v4zm4 4h-2v-2h2v2zm0-4h-2v-4h2v4z');
+    svgPath.setAttribute('d', 'M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z');
     svgPath.setAttribute('fill', 'currentColor'); // Use current text color as fill
     
     // Append path to SVG element
